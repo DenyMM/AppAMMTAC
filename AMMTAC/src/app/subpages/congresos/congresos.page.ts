757ed8f4e7697b377495c25b33fdaec318046c21
Jesús <jesus_mx12@hotmail.com>
2019-09-04 15:16:45 -0500

    Correcion por Jesus
    Descarga de programa funcionando

--- AMMTAC/src/app/subpages/congresos/congresos.page.ts
+++ AMMTAC/src/app/subpages/congresos/congresos.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer/ngx';
 import { Platform } from '@ionic/angular';
-import { File } from '@ionic-native/file/ngx';
+import { File } from '@ionic-native/File/ngx';
 import { FileOpener } from '@ionic-native/file-opener/ngx';
 import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 
@@ -31,7 +31,7 @@ export class CongresosPage {
  
     if (this.platform.is('android')) {
       let fakeName = Date.now();
-      this.file.copyFile(filePath, 'programa_final.pdf', this.file.dataDirectory, `${fakeName}.pdf`).then(result => {
+      this.file.copyFile(filePath, 'Programa_2019.pdf', this.file.dataDirectory, `${fakeName}.pdf`).then(result => {
         this.fileOpener.open(result.nativeURL, 'application/pdf')
           .then(() => console.log('File is opened'))
           .catch(e => console.log('Error opening file', e));
@@ -41,7 +41,7 @@ export class CongresosPage {
       const options: DocumentViewerOptions = {
         title: 'Programa'
       }
-      this.document.viewDocument(`${filePath}/programa_final.pdf`, 'application/pdf', options);
+      this.document.viewDocument(`${filePath}/Programa_2019.pdf`, 'application/pdf', options);
     }
 	}
 	
